f178de62cdbc843f9daa9045fcb5abc7a4f2680d
cyco130 <cyco130@gmail.com>
2022-10-28 18:20:18 +0300

    chore: use tabs and add unit tests

--- tests/bench/vite.config.ts
+++ tests/bench/vite.config.ts
@@ -2,16 +2,16 @@ import { defineConfig } from "vite";
 import { cjsInterop } from "vite-plugin-cjs-interop";
 
 export default defineConfig({
-  build: {
-    ssr: "src/index.js",
-    sourcemap: true,
-  },
-  ssr: {
-    external: ["cjs-test-package"],
-  },
-  plugins: [
-    cjsInterop({
-      dependencies: ["cjs-test-package"],
-    }),
-  ],
+	build: {
+		ssr: "src/index.js",
+		sourcemap: true,
+	},
+	ssr: {
+		external: ["cjs-test-package"],
+	},
+	plugins: [
+		cjsInterop({
+			dependencies: ["cjs-test-package"],
+		}),
+	],
 });
