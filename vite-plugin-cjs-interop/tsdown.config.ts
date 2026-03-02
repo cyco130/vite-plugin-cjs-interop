7b9c0a901bdd570f9a010bcc4ac4ca38bcdd461b
Fatih Aygün <cyco130@gmail.com>
2026-03-02 12:47:04 +0000

    chore: migrate to tsdown

--- vite-plugin-cjs-interop/tsdown.config.ts
+++ vite-plugin-cjs-interop/tsdown.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
 
 export default defineConfig([
 	{
 		entry: ["./src/index.ts"],
+		fixedExtension: false,
 		format: ["esm", "cjs"],
 		platform: "node",
 		target: "node20",
+		sourcemap: true,
 		dts: true,
 	},
 ]);
